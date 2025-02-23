98db66bec4ac99de356a065a55f1863e348d9747
trojan-1 <arinomo4@gmail.com>
2025-02-23 11:37:10 +0100

    Add files via upload

---
+++ MLapp.py
@@ -0,0 +1,22 @@
+
+import pickle
+import streamlit as st
+import pandas as pd
+
+# load the file that contains the model (model.pkl)
+with open("model.pkl", "rb") as f:
+  model = pickle.load(f)
+
+# give the Streamlit app page a title
+st.title("Customer Churn")
+
+# input widget for getting user values for X (feature matrix value)
+CustServCalls = st.slider("CustServCalls", min_value=0, max_value=9, value=3)
+DayMins = st.slider("DayMins", min_value=0, max_value=350.8, value=70)
+DataUsage = st.slider("DataUsage", min_value=0, max_value=5.4, value=3)
+
+# After selesting CustServCalls, DayMins, DataUsage the user then submits the price value
+if st.button("Predict"):
+  # take the price value, and format the value the right way
+  prediction = model.predict([[CustServCalls, DayMins, DataUsage]])[0].round(2)
+  st.write("The customer is likely to", prediction, "in the coming quarter")
